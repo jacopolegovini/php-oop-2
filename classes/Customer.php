5f26f5dbdc8ba9dccf7395c17b0fa397f6492ed4
jacopolegovini <legovini96@gmail.com>
2024-10-10 17:30:00 +0200

    customer class created

---
+++ classes/Customer.php
@@ -0,0 +1,25 @@
+<?php
+
+class Customer
+{
+    public $account;
+    public $workingCreditCard;
+
+    public function __construct(bool $account, bool $workingCreditCard)
+    {
+        $this->account = $account;
+        $this->workingCreditCard = $workingCreditCard;
+    }
+
+    public function hasAnAccount()
+    {
+        $this->account = false;
+        if ($this->account = true) return true;
+    }
+
+    public function isCreditCardExpired()
+    {
+        $this->workingCreditCard = false;
+        if ($this->workingCreditCard = true) return true;
+    }
+}
